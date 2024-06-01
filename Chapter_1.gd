561ddb4aaf19e8a9c6901c94e113a73e02d8829e
Inari Sorakara <160137131+inariSorakara@users.noreply.github.com>
2024-06-01 18:58:05 -0300

    Add files via upload
    Chapter 1's Script

---
+++ Chapter_1.gd
@@ -0,0 +1,11 @@
+#Scenes to add
+''' Two options for scene 1
+Scene 1 A = We meet a young man who works in a car retailer. A curious looking man comes inside, this is Niall R. Thotep, our story's big bad. Of course, the reader doesn't know this.
+We find out our young man is bullied by his boss. Niall takes notice of this. After some shenanigans of Niall pretending to be interested in buying a car he ends up convinging/manipulating the young man into killing his boss
+The young man is about to let out a cry/roar when we cut to scene 2
+Scene 1 B = We see a website page with a video player window. A mouse clicks on the video and it starts playing. We listen someone greet the viewer to their vlog. This is Hanae Tamami, our protagonist.
+Hanae, with visuals on the screen, explains about her city "Liancourt City", a land that centuries ago came out of the ocean between Japan and Korea.
+She talks about the story of how Korea
+'''
+
+
