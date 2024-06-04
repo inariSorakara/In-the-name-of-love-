74d05a211a273b8d6c5a679b5a10b51baa800df6
Inari Sorakara <160137131+inariSorakara@users.noreply.github.com>
2024-06-03 22:46:23 -0300

    Add files via upload

--- Chapter_1.gd
+++ Chapter_1.gd
@@ -5,7 +5,16 @@ We find out our young man is bullied by his boss. Niall takes notice of this. Af
 The young man is about to let out a cry/roar when we cut to scene 2
 Scene 1 B = We see a website page with a video player window. A mouse clicks on the video and it starts playing. We listen someone greet the viewer to their vlog. This is Hanae Tamami, our protagonist.
 Hanae, with visuals on the screen, explains about her city "Liancourt City", a land that centuries ago came out of the ocean between Japan and Korea.
-She talks about the story of how Korea
+She talks about the story of how many pilgrims tried to colonize the island in name of their country, mainly japanese and koreans; but also some european countries and even people from United States. 
+That was until the  AKUMU appeared. Vicious creatures that take many forms. Some think they are made of black magic, some believe they came from other world.
+Some believe they are made of people's nightmares. The rich and the well-established were the first to ran away of the islands. The poor and those who had spent everything they had to move to the village had to stay behind.
+It all seemed lost. The crops were stolen or destroyed. The people were hunted at night. The residents of Liancourt gave up all hope.
+But once again, that was until SHE appeared.
+Dazzling, brightful, bringing a bag full of hope and smiles. Weilding a her Leek sword and leaf dress. Yumemi Midori burst into action!
+She defeated the Akumu's every time they appeared. Be it demons, kaijus, monster of any kind. The people of Liancourt city now had a fighting chance with Yumemi Midori by their side.
+No matter how many times she was knock down, she always got up again. Always with a smile.
+With the Akumu brought to the brink of extintion, Pretty Guardian Yumemi Midori declared herself the guardian of Liancourt City and prosperity came back to the islands.
+We see a still drawing of Yumemi Midori posing as it fades into scene 2.
 '''
 
 
